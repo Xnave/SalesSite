ac3b797b40390177d00d5b3fffe3546b86bc12a6
PrinceDM <ddoronbboy@gmail.com>
2015-08-12 23:08:06 +0300

    1st beta style

---
+++ resources/views/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Sales</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <link href='http://fonts.googleapis.com/css?family=Exo+2:400,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.js"></script>
+
+                <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    </head>
+
+    <body>
+        
+        <div class="warrper">
+
+
+            <?php include_once 'nav.php'; ?>
+
+            <div class="headingSection paper"> 
+                <h1>Sales</h1>    
+                <h2>Wellcome to the sales page</h2>
+            </div>
+
+            <div class="sales paper">
+
+                <div class="row">
+
+                  <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <div class="saleBrande" style="background-image: url(images/2000px-Zara_Logo_2.svg.png);">
+                        </div>
+                        <div class="saleText more">
+                            <p class="low">30% Discount</p>
+                        </div>
+                    </a>
+                  </div>
+
+                    <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <div class="saleBrande" style="background-image: url(images/logo-1.png);">
+                        </div>
+                        <div class="saleText more">
+                            <p class="middle">50% Discount</p>
+                        </div>
+                    </a>
+                  </div>
+
+                    <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+<!--                        <div class="icon icon-burst"></div>-->
+                        <div class="saleBrande" style="background-image: url(images/GetImg.png);">
+                        </div>
+                        <div class="saleText more">
+                            <p class="middle">50% Discount</p>
+                        </div>
+                    </a>
+                  </div>
+                    
+                    <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+<!--                        <div class="icon icon-burst"></div>-->
+                        <div class="saleBrande" style="background-image: url(images/GetImg.png);">
+                        </div>
+                        <div class="saleText more">
+                            <p class="middle">50% Discount</p>
+                        </div>
+                    </a>
+                  </div>
+                    
+                    
+                    
+                    
+<!--
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+                    <div class="icon icon-sale"></div>
+-->
+                </div>
+            
+        </div>
+            
+            </div>
+        
+        <footer>&copy; Sellnco <?php echo date("Y"); ?></footer>
+        
+    </body>
+
+</html>
