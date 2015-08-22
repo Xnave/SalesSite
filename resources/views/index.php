03bd9b3a695e3a4a0be4739cc0c5b6e6acda578b
Nave <naveine97@gmail.com>
2015-08-22 20:24:33 +0300

    admin authentication & 404 handling

--- resources/views/index.php
+++ resources/views/index.php
@@ -12,7 +12,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.js"></script>
 
-                <link rel="stylesheet" type="text/css" href="css/style.css">
+                <link rel="stylesheet" type="text/css" href="css/app.css">
 
     </head>
 
